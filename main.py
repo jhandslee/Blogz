110004328c4c5d2f11e1e60759146a30a965898b
Jesse Hansley <jhandslee@gmail.com>
2019-01-03 09:25:02 -0500

    adding project files to repository

---
+++ main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request, redirect, render_template, session, flash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_ECHO'] = True
+db = SQLAlchemy(app)
+#app.secret_key = '50-QXZ!6jD'
+
+class Blog(db.Model):
+	
+	id = db.Column(db.Integer, primary_key=True)
+	entry = db.Column(db.String(120))
+	body = db.Column(db.String(600))
+
+	def __init__(self, entry, body):
+		self.entry = entry
+		self.body = body
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+	if request.method == 'POST':
+		entry =	request.form['entry']
+		body = request.form['body']
+		n_entry = Blog(entry, body)
+		db.session.add(n_entry)
+		db.session.commit()
+
+	n_entry = Blog.query.filter().all()
+	return render_template('newpost.html', title="New Blog Entries", n_entry=n_entry)
+	
+@app.route('/blog', methods=['POST', 'GET'])
+def blog():
+
+	n_entry = Blog.query.filter().all()
+	return render_template('blog.html', title="Blog Entries", n_entry=n_entry)
+
+@app.route('/newpost', methods=['POST', 'GET'])
+def newpost():
+
+	if request.method == 'POST':
+		entry =	request.form['entry']
+		body = request.form['body']
+		n_entry = Blog(entry, body)
+		db.session.add(n_entry)
+		db.session.commit()
+
+	n_entry = Blog.query.filter().all()
+	return render_template('newpost.html', title="New Blog Entries", n_entry=n_entry)
+
+if __name__ == '__main__':
+	app.run()
