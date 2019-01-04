8d255d952fbd44110fa4baeab5d1ea4defa46cee
Jesse Hansley <jhandslee@gmail.com>
2019-01-03 19:25:17 -0500

    update newpost file to add functionality

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app.config['DEBUG'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:password@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
-#app.secret_key = '50-QXZ!6jD'
+app.secret_key = '50-QXZ!6jD'
 
 class Blog(db.Model):
 	
